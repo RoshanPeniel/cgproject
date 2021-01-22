f31c95d50f187361db40cf030961333acfc135d0
RoshanPeniel <67621526+RoshanPeniel@users.noreply.github.com>
2021-01-22 17:32:21 +0530

    2

---
+++ indisign.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "crewgenerator";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$uname = $_POST['uname'];
+
+
+$sql = "INSERT INTO indiuser (uname ,email, password)
+VALUES ('$uname', '$email', '$pass')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
